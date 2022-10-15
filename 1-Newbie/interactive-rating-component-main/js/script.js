286dee447b29e3a4ec71e31d6954eeec56af437c
VincenzoMuolo <vincenzo.muolo96@gmail.com>
2022-10-15 16:39:32 +0200

    Interactive rating component project completed
    Handled onclick event, added some animation.

--- 1-Newbie/interactive-rating-component-main/js/script.js
+++ 1-Newbie/interactive-rating-component-main/js/script.js
@@ -5,3 +5,26 @@ function clickHandler(element){
     }
     document.getElementById(element.id).classList.add("active");
 }
+function saveRating(){
+    let i=1;
+    let cardSize= document.getElementById("mainCard");
+    while(i<=5){
+        if(document.getElementById(i).classList.contains("active")){
+            document.getElementById("userVote").textContent=i;
+            document.getElementById("iconContainer").classList.toggle("animation-out");
+            document.getElementById("textCard").classList.toggle("animation-out");
+            document.getElementById("ratingBox").classList.toggle("animation-out");
+            document.getElementById("submit").classList.toggle("animation-out");
+            setTimeout(
+                function animTime(){
+                    document.getElementById("iconContainer").style.display="none";
+                    document.getElementById("textCard").style.display="none";
+                    document.getElementById("thanksCard").style.display="flex";
+                    document.getElementById("thanksCard").classList.toggle("animation-in");
+                }
+            ,1500);
+            break;
+        }
+        else i++;      
+    }
+}
