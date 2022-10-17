23b78ae62c7253cd066d5619a3e8a5e380063b99
VincenzoMuolo <vincenzo.muolo96@gmail.com>
2022-10-17 17:23:47 +0200

    Fix: first commit

--- homepage/js/script.js
+++ homepage/js/script.js
@@ -1,33 +1,75 @@
 function createBackground(){
-    let screenX, screenY, itemArea, nTileX, nTileY, tileHTML,
-    i=0,
-    container= document.getElementById("mainContainer");
+    var cellDimension=0;
+    /*Adjust cell to screen dimension*/
+    let width=document.body.clientWidth;
+    switch(true){
+        case width>=2560:
+            cellDimension= 150;
+            break;
+        case width>=1980:
+            cellDimension= 80;
+            break;
+        case width>=768:       
+            cellDimension= 70;
+            break;
+        default :
+            cellDimension= 50;
+            break;
+    }
+    createGrid(cellDimension);
+    window.addEventListener("resize", function(){   
+        this.location.reload();
+        createGrid(cellDimension);   
+    });
+}
 
-    screenX= window.screen.height;
-    screenY= window.screen.width;
-    itemArea=document.getElementsByClassName("");
-    nTileX= Math.trunc(screenX/itemArea);
-    nTileY= Math.trunc(screenY/itemArea);
-    container.style.gridTemplateColumns= "repeat("+nTileX+",minmax(50px, 1fr))";
-    //container.style.gridTemplateRows= "repeat("+nTileY+",minmax(50px, 1fr))";
-    do{
-        tileHTML="<div id='item' class='container-item'></div>";
-        container.insertAdjacentHTML("beforeend",tileHTML);
-        i++;
-        console.log("i");
-    }while(nTileX*nTileY>i);
-    
-    
-    
-    window.addEventListener("resize", function(){
-        screenX= window.screen.height;
-        screenY= window.screen.width;
-        itemArea= 50;
-        nTileX= Math.trunc(screenX/itemArea);
-        nTileY= Math.trunc(screenY/itemArea);
-        console.log("\nscreenX : "+screenX+
-        "\nscreenY : "+screenY+
-        "\nnumber of tiles Y : "+nTileY+
-        "\nnumber of tiles X : "+nTileX);
-    }); 
+function createGrid(cellDim){
+    let column= Math.floor(document.body.clientWidth/cellDim),
+    row= Math.floor(document.body.clientHeight/cellDim);
+    console.log("Column : "+column+
+                "\nbody width : "+document.body.clientWidth+
+                "\nRows : "+row+
+                "\nbody height : "+document.body.clientHeight);
+    let container= document.getElementById("container");
+    let startcol,endcol,endrow;
+    switch(true){
+        case column>=20 :
+            startcol=6;
+            endcol=column-4;
+            break;
+        case column>=15 :
+            startcol=4;
+            endcol=column-2;
+            break;
+        case column>=10 :
+            startcol=3;
+            endcol=column-1;
+            break;
+        case column<=10 :
+            startcol=2;
+            endcol=column;
+            break;
+        default :
+            startcol=2;
+            endcol=column-2;
+        }
+        switch(true){
+            case row>=20 :
+                endrow=row-1;
+                break;
+            default:
+                endrow=row-1;
+        }
+
+    container.style.setProperty("--endRow", endrow);
+    container.style.setProperty("--startCol", startcol);
+    container.style.setProperty("--endCol", endcol);
+    container.style.setProperty("--columns", column);
+    container.style.setProperty("--columnNav", column+1);
+    container.style.setProperty("--rows",row);
+    let cell="<div class='cell'></div>";
+
+    for(let i=0;i<(column*row);i++){
+        container.innerHTML+=cell;
+    }
 }
