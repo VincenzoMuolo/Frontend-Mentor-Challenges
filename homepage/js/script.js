79a116c7367c0eb934f222b16756170e08ffa776
VincenzoMuolo <vincenzo.muolo96@gmail.com>
2022-10-20 12:16:15 +0200

    New responsive homepage (mobile friendly)
    New homepage for Frontend mentor challenges repo.

--- homepage/js/script.js
+++ homepage/js/script.js
@@ -1,75 +1,49 @@
-function createBackground(){
-    var cellDimension=0;
-    /*Adjust cell to screen dimension*/
-    let width=document.body.clientWidth;
-    switch(true){
-        case width>=2560:
-            cellDimension= 150;
-            break;
-        case width>=1980:
-            cellDimension= 80;
-            break;
-        case width>=768:       
-            cellDimension= 70;
-            break;
-        default :
-            cellDimension= 50;
-            break;
-    }
-    createGrid(cellDimension);
+
+function setHeight(){
+    let navHeight=document.getElementById("nav").offsetHeight,
+        availPage=(screen.availHeight-navHeight);
+    if(screen.availHeight>900)  availPage=(availPage*0.7);
+    else    availPage=(availPage*0.9);
+    document.getElementById("container").style.height=availPage+"px";
+    /*Readapt if the screen change*/
     window.addEventListener("resize", function(){   
         this.location.reload();
-        createGrid(cellDimension);   
+        this.setTimeout(function rest(){
+            document.getElementById("container").style.height=availPage+"px";
+        },1000);  
     });
+
+}
+const diff= ["newbie","junior","intermediate","advanced"];
+
+function expandChosenDif( chosedDiff ){
+    if(localStorage.getItem('lastChoise')==chosedDiff.id){
+        /*The user want to close the difficulty page selected,
+            so i need to recreate the initial situation*/
+        transformRows(chosedDiff.id, 1);
+        localStorage.setItem('lastChoise',' ');
+    }
+    else {
+        /*The user select a different difficulty than before,
+            so i have to make disappear the other ones*/  
+        transformRows(chosedDiff.id, 0);
+        localStorage.setItem('lastChoise',chosedDiff.id);
+    }
+
 }
 
-function createGrid(cellDim){
-    let column= Math.floor(document.body.clientWidth/cellDim),
-    row= Math.floor(document.body.clientHeight/cellDim);
-    console.log("Column : "+column+
-                "\nbody width : "+document.body.clientWidth+
-                "\nRows : "+row+
-                "\nbody height : "+document.body.clientHeight);
-    let container= document.getElementById("container");
-    let startcol,endcol,endrow;
-    switch(true){
-        case column>=20 :
-            startcol=6;
-            endcol=column-4;
-            break;
-        case column>=15 :
-            startcol=4;
-            endcol=column-2;
-            break;
-        case column>=10 :
-            startcol=3;
-            endcol=column-1;
-            break;
-        case column<=10 :
-            startcol=2;
-            endcol=column;
-            break;
-        default :
-            startcol=2;
-            endcol=column-2;
+function transformRows(checkDiff, value){
+    diff.forEach(element => {
+        if(element!=checkDiff){
+            document.getElementById(element).style.height="0%";
+            document.getElementById(element).style.opacity=0;
+            document.getElementById(element).style.fontSize="0rem";
+            document.getElementById(element).style.transition=
+                "400ms height ease-out, 300ms font-size ease-in, 400ms opacity ease-out";
+        }else{
+            document.getElementById(element).style.height="95%";
+            document.getElementById(element).style.transition=
+                "400ms height ease-in-out";
         }
-        switch(true){
-            case row>=20 :
-                endrow=row-1;
-                break;
-            default:
-                endrow=row-1;
-        }
-
-    container.style.setProperty("--endRow", endrow);
-    container.style.setProperty("--startCol", startcol);
-    container.style.setProperty("--endCol", endcol);
-    container.style.setProperty("--columns", column);
-    container.style.setProperty("--columnNav", column+1);
-    container.style.setProperty("--rows",row);
-    let cell="<div class='cell'></div>";
-
-    for(let i=0;i<(column*row);i++){
-        container.innerHTML+=cell;
-    }
+    });
 }
